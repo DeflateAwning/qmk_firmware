94a2dde3fac3944be097cc3fb35e187bc4af71f0
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2022-10-05 20:46:11 -0600

    Add QWERTY mode for hotkeys

--- keyboards/keebio/iris/keymaps/DeflateAwning/keymap.c
+++ keyboards/keebio/iris/keymaps/DeflateAwning/keymap.c
@@ -1,6 +1,7 @@
 #include QMK_KEYBOARD_H
 enum my_layers {
     _DVORAK,
+    _QWERTY, // access with button on the _LOWER layer
     _LOWER, // access with left-hand layer key
     _RAISE, // access with right-hand layer key
     _VI_ARROWS, // access by pressing "OEU" as a chord/combo
@@ -16,8 +17,10 @@ uint16_t COMBO_LEN = COMBO_COUNT;
 
 #define RAISE MO(_RAISE)
 #define LOWER MO(_LOWER)
+#define B_QWE DF(_QWERTY) // set default base layer
+#define B_DVO DF(_DVORAK) // set default base layer
 
-#define ALTTAB LALT(KC_TAB)
+#define ALTTAB LALT(KC_TAB) // FIXME needs to be a macro or something that holds down the ALT key after pressing tab
 
 enum custom_keycodes {
    BSP_DEL = SAFE_RANGE
@@ -39,15 +42,30 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
 
+  
+  [_QWERTY] = LAYOUT(
+  //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
+     KC_ESC,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    BSP_DEL,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,                               KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_DEL,
+  //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,                               KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, B_DVO,
+  //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
+     KC_LSFT, KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    _______,          _______, KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_ENT,
+  //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
+                                    LOWER,   KC_LGUI,  KC_LSFT,                   KC_SPC,  KC_RALT, RAISE
+                                // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
+  ),
+
   [_LOWER] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
      KC_GRAVE, KC_EXLM, KC_AT,  KC_HASH, KC_DLR,  KC_PERC,                            KC_CIRC, KC_AMPR, KC_ASTR, KC_LPRN, KC_RPRN, BSP_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
      ALTTAB,   KC_1,    KC_2,    KC_3,    KC_4,    KC_5,                               KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_DEL,
   //├────────┼────────┼────────┼────────┼────────┼────────┤                          ├────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LCTL, _______, _______, _______, KC_BSLS, KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_BSPC,
+     KC_LCTL, KC_LEFT,  KC_UP,  KC_DOWN, KC_RGHT, KC_LBRC,                            KC_RBRC, KC_P4,   KC_P5,   KC_P6,   KC_PLUS, KC_BSPC,
   //├────────┼────────┼────────┼────────┼────────┼────────┼────────┐        ┌────────┼────────┼────────┼────────┼────────┼────────┼────────┤
-     KC_LSFT, _______, _______, _______, _______, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_PIPE,
+     KC_LSFT, _______, _______, B_QWE  , KC_BSLS, KC_LCBR, KC_LPRN,          KC_RPRN, KC_RCBR, KC_P1,   KC_P2,   KC_P3,   KC_MINS, KC_PIPE,
   //└────────┴────────┴────────┴───┬────┴───┬────┴───┬────┴───┬────┘        └───┬────┴───┬────┴───┬────┴───┬────┴────────┴────────┴────────┘
                                     _______, KC_LGUI, KC_LSFT,                    KC_DEL,  KC_RALT, KC_P0
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
@@ -67,6 +85,7 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
                                     _______, KC_LGUI, KC_LSFT,                   _______, KC_RALT, _______
                                 // └────────┴────────┴────────┘                 └────────┴────────┴────────┘
   ),
+
 
   [_VI_ARROWS] = LAYOUT(
   //┌────────┬────────┬────────┬────────┬────────┬────────┐                          ┌────────┬────────┬────────┬────────┬────────┬────────┐
--- keyboards/keebio/iris/keymaps/DeflateAwning/readme.md
+++ keyboards/keebio/iris/keymaps/DeflateAwning/readme.md
@@ -3,18 +3,19 @@
 This keyboard layout was forked from the `keebio/iris -> dvorak` mapping, with inspiration from a few other layouts.
 
 ## Notable Changes
+* Default base layer is Dvorak, but QWERTY mode can be activated to use hotkeys easier (e.g., cut/copy/paste/refresh/bold).
 * The lshift key is on the left thumb, allowing it to act as capslock for acronyms easily, and making it accessible for hotkeys.
 * Modifier keys (Ctrl, Shift, Win, Alt, etc.) are kept mostly constant between layers.
 * Pressing LOWER+Tab sends the Alt+Tab hotkey to cycle through Windows.
-* When SDF (OEU) are all pressed (which rarely happens in words as OEU), the right hand home row works as arrow keys/home/end.
+* When SDF (OEU) are all pressed (which rarely happens in words as OEU), the right hand home row works as arrow keys/home/end (similar to in VI).
     * Shift-selecting with the arrow keys, and jumping by word with the arrow keys and Ctrl is easy.
 * SHIFT+Backpace sends forwards DELETE
 
 ## To Do Changes
-* Game mode, especially for copy/paste/cut/undo hotkeys, which are very useful on the left hand.
 * Setup Plover via Gemini PR protocol.
 * Create PDF cheat sheet to print.
 * Holding the LOWER key doesn't continue holding Alt for Alt+Tab
+* Striking the left-thumb shift key very fast should type a space.
 
 ## Building
 
