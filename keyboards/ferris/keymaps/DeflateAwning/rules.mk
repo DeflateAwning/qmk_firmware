9c50763296b6751e55a2f0a036acf915cfc6d0a5
DeflateAwning <11021263+DeflateAwning@users.noreply.github.com>
2025-05-19 00:03:20 -0600

    Create basic working Sweep configuration

---
+++ keyboards/ferris/keymaps/DeflateAwning/rules.mk
@@ -0,0 +1,7 @@
+# Depending on hardware either 'CONVERT_TO=sparkfun_pm2040' or 'CONVERT_TO=rp2040_ce' should be used instead.
+# Guide: https://docs.qmk.fm/feature_converters#pro-micro
+
+# Deprecated: CONVERT_TO = promicro_rp2040e
+# Depending on hardware either 'CONVERT_TO=sparkfun_pm2040' or 'CONVERT_TO=rp2040_ce' should be used instead.
+
+CONVERT_TO = rp2040_ce
